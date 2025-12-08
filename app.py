6dbab7a0020e207cecb4a109190e02019999e7e8
kumar-kiran-24 <kirankumar26650@gmail.com>
2025-12-08 16:32:23 +0530

    routers for rag chatbot

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-import os
-import streamlit as st
-from dotenv import load_dotenv
+from flask import Flask,request,Response,render_template,jsonify
 
-from langchain_groq import ChatGroq
-from langchain_text_splitters import TextSplitter
+from main import Main
+
+obj=Main()
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/from_link",methods=["POST","GET"])
+def from_link():
+    
+    if request.method=="POST":
+        data=request.form.get("user_link")
+        try:
+            response=obj.main_for_web(link=data,question="explain hotw the php code is ")
+            return response
+        except:
+            print("error")
+            return "error processing link",500
+
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=1111,debug=True)
+    
 
 
